e78f19aa5c5f76e2dc7a3c5f7012288dc8cd88cf
alberto <albertocstll70@gmail.com>
2022-07-12 00:43:17 +0200

    N1S1T2CastilloAlberto/

--- N1S1T2CastilloAlberto/src/s1t2n1exercici1/MainT2N1Exercici1.java
+++ N1S1T2CastilloAlberto/src/s1t2n1exercici1/MainT2N1Exercici1.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class MainT2N1Exercici1 {
 
-	public static void main(String[] args) throws VentaBuidaException  {
+	public static void main(String[] args) throws VentaBuidaException {
 
 		Producto p1 = new Producto(" TV", 650);
 		Producto p2 = new Producto(" Ordenador", 1560);
@@ -17,25 +17,11 @@ public class MainT2N1Exercici1 {
 
 		ArrayList<Producto> listProductoVacio = new ArrayList<Producto>();
 
-		
-	
-		
-			
-			
-			
-			
-			Venta	compra = new Venta(listProductoVacio );
-			
-			
-		
-		
+		Venta compra = new Venta(listProductoVacio);
 
-		
-	//	Venta compra = new Venta(listProducto);
-		  
-		//System.out.println(compra);
-		  
-		 
+		// Venta compra = new Venta(listProducto);
+
+		// System.out.println(compra);
 
 		/*
 		 * Escribe el código necesario para generar y capturar una excepción de tipo
--- N1S1T2CastilloAlberto/src/s1t2n1exercici1/Venta.java
+++ N1S1T2CastilloAlberto/src/s1t2n1exercici1/Venta.java
@@ -7,47 +7,36 @@ public class Venta {
 	private float precioTotal;
 	private ArrayList<Producto> listProducto = new ArrayList<Producto>();
 
-	public Venta(ArrayList<Producto> listProducto)    {
+	public Venta(ArrayList<Producto> listProducto) {
 
 		this.listProducto = listProducto;
-		
-			
-			
-					try {
-						this.precioTotal = calcularTotal();
-					} catch (VentaBuidaException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				
-					
-			
-				
-			
-		
+
+		try {
+			this.precioTotal = calcularTotal();
+		} catch (VentaBuidaException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
-	public float calcularTotal() throws VentaBuidaException  {
-		
+	public float calcularTotal() throws VentaBuidaException {
+
 		int size = this.listProducto.size();
 		float total = 0;
 
-	if(size==0) {
-		throw new VentaBuidaException();
-		
-	}else {
-	
+		if (size == 0) {
+			throw new VentaBuidaException();
 
-		
-		for (int i = 0; i < size; i++) {
-			
+		} else {
 
-			total = total + listProducto.get(i).getPrecio();
+			for (int i = 0; i < size; i++) {
+
+				total = total + listProducto.get(i).getPrecio();
+			}
 		}
-	}
 		return total;
-		
+
 	}
 
 	@Override
